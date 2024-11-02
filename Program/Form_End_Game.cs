d30c40c0d50f1b8891f588e3cd4d4e042628e8d1
Thuan1786 <huynhngocthuan736.2020@gmail.com>
2024-11-02 13:22:28 +0700

    abc

---
+++ Program/Form_End_Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program
+{
+    public partial class Form_End_Game : Form
+    {
+        public Form_End_Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_End_Game_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
